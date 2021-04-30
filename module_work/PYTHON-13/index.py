b0a4bedb0cab88681f301fffddcbc890b51ce333
daimond <daimond025@yandex.ru>
2021-04-30 20:14:57 +0300

    Второй модуль - законченное решение!

---
+++ module_work/PYTHON-13/index.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta
+
+
+
+
+# from datetime import datetime
+# datetime_list = [
+#     datetime(2019, 7, 7, 18, 59, 6),
+#     datetime(2019, 7, 7, 19, 0, 2),
+#     datetime(2019, 7, 7, 19, 1, 4)
+# ]
+# report_seconds = []
+# for item in datetime_list:
+#     report_seconds.append(item.second)
+#
+# # total_time =
+# print(sum(report_seconds))
+
+
+# dt_list = ['2019-07-07T18:59:06', '2019-07-07T19:00:02', '2019-07-07T19:01:04']
+# datetime_list = []
+# for item in dt_list:
+#     dt = datetime.strptime(item, '%Y-%m-%dT%H:%M:%S')
+#     datetime_list.append(dt)
+
+
+# date_string = '2019-07-07T18:59:33'
+# dt = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%S')
+# date_format =dt.strftime('%d.%m.%Y')
+# print(date_format)
+
+# dt = datetime(2019, 4, 1, 18, 59, 44)
+# print(dt.strftime('%Y-%m-%d'))
+
+# date_string = '05.05.2019 21:00'
+# dt = datetime.strptime(date_string, '%d.%m.%Y %H:%M')
