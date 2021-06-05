2aba1d4fcbfba21d8e7da77b27d58cc1528a5271
daimond <daimond025@yandex.ru>
2021-06-05 14:43:28 +0300

    Третий модуль - готовое решение

--- module_3/sample/index.py
+++ module_3/sample/index.py
@@ -20,42 +20,62 @@ class PrepareDate():
 
     def processData(self):
         df_output = self.df_output.copy()
-        # убираем не нужные для модели признаки
-        df_output.drop(['Restaurant_id', 'ID_TA', ], axis=1, inplace=True)
+
+        df_output.drop(['ID_TA', ], axis=1, inplace=True)
+        # признак сети - одинаковые Restaurant_id для ресторанов
+        # УБИРАЕМ - так как новый признак никак не повлиял на результаьт в результате опыта
+        # df_output = self.netRestaurant(df_output)
 
         # Далее заполняем пропуски
         df_output['Number of Reviews'].fillna(0, inplace=True)
 
-
         # нормировка значения Ranking
         df_output = self.normilizeRanking(df_output)
 
+        # обработка цен - заполяняем цены согласно словарю
         df_output = self.ProcessPrice(df_output)
-        #  обрботка  кухни
+        #  обрботка  кухни - вводим новый признак (количество кухонь) + распарсиваем строку кухонь
         df_output = self.ProcessCuisine(df_output)
-        # url
+
+        # url - обработка url - извлекаем параметр
         df_output = self.ProcessURL(df_output)
 
-        #  обрботка отзывов
+        #  обрботка отзывов - вводим новые признаки:
+        #  - средняя длина отзыва
+        #  - количество слов (превосходные отзывы - поиск по словам)
+        #  - взлечени дат - вычисдение разницы между датами отыва
+        # Убираем эти параметры - большое время обработки и малый вклад в точность прогнозирования (по результатам опыта)
         # df_output = self.ProcessReviews(df_output)
 
+        # вводим переменные dummi как города так и стили кухонь - такое действие позволяет не выводить новые признаки в
+        # каких городах пицерии/бары/кафетерии и т.п. !!!
         df_output = pd.get_dummies(df_output, columns=['City', 'Cuisine Style', 'URL_TA'], dummy_na=True)
 
-        object_columns = [s for s in df_output.columns if df_output[s].dtypes == 'object']
+        # убираем не нужные для модели признаки
+        object_columns = [s for s in df_output.columns if df_output[s].dtypes == 'object' and  s !='Restaurant_id' ]
         df_output.drop(object_columns, axis=1, inplace=True)
 
         return df_output
 
+    def netRestaurant(self, data):
+        list_net = (data['Restaurant_id'].value_counts()[data['Restaurant_id'].value_counts() > 1].index)
+        data['net'] = data.apply(lambda row: 1 if row['Restaurant_id'] in list_net else 0, axis=1)
+        return data
+
+
     def ProcessURL(self, data):
         data['URL_TA'] = data.apply(lambda row: str(row['URL_TA']).split('-')[1], axis=1)
         return data
-    def ProcessPrice(self,data ):
-        PriceRange = {''
-                      '$$$$': 3,
-                      '$$ - $$$': 2,
-                      '$': 1 }
+
+    def ProcessPrice(self, data):
+        PriceRange = {
+            '$$$$': 3,
+            '$$ - $$$': 2,
+            '$': 1
+        }
         data['Price Range'] = data['Price Range'].map(PriceRange).fillna(0)
         return data
+
     def ProcessReviews(self, data):
         data['Reviews'] = data['Reviews'].str.replace('[[', '', regex=False).str.replace(']]', '', regex=False) \
             .str.replace('[', '', regex=False).str.replace("'", '', regex=False).str.strip()
@@ -92,13 +112,13 @@ class PrepareDate():
 
                     #  по словам
                     good_words = ['good', 'yummy', 'fine', 'great', 'tasty',
-                          'satisfaction', 'amazing', 'nice', 'best',
-                          'friendly', 'pleasant', 'exellent', 'loved',
-                          'love', 'lovely', 'welcoming', 'wonderful',
-                          'perfect', 'delicious', 'favourite', 'sweet',
-                          'yum', 'adequate', 'happy', 'beautiful', 'liked', 'like']
+                                  'satisfaction', 'amazing', 'nice', 'best',
+                                  'friendly', 'pleasant', 'exellent', 'loved',
+                                  'love', 'lovely', 'welcoming', 'wonderful',
+                                  'perfect', 'delicious', 'favourite', 'sweet',
+                                  'yum', 'adequate', 'happy', 'beautiful', 'liked', 'like']
 
-                    str_reviews =  str(row['Reviews'][0]).lower()
+                    str_reviews = str(row['Reviews'][0]).lower()
                     count = 0
                     for word in good_words:
                         if word in str_reviews:
@@ -121,6 +141,7 @@ class PrepareDate():
         data = data.drop(['ReviewDate1', 'ReviewDate2'], axis=1)
 
         return data
+
     def ProcessCuisine(self, data):
         # TODO обработка кухни
         data['Cuisine Style'] = data['Cuisine Style'].str.replace('[', '', regex=False).str.replace(']', '',
@@ -159,7 +180,6 @@ df_test['sample'] = 0
 df_test['Rating'] = 0
 data = df_test.append(df_train, sort=False).reset_index(drop=True)
 
-
 DATA_DIR = os.getcwd() + '/kaggle/input'
 df_train = pd.read_csv(DATA_DIR + '/main_task.csv')
 df_test = pd.read_csv(DATA_DIR + '/kaggle_task.csv')
@@ -167,13 +187,18 @@ df_train['sample'] = 1
 df_test['sample'] = 0
 df_test['Rating'] = 0
 
+
 data = df_test.append(df_train, sort=False).reset_index(drop=True)
+
 
 df_object = PrepareDate(data)
 df_preproc = df_object.processData()
 
-train_data = df_preproc.query('sample == 1').drop(['sample'], axis=1)
+train_data = df_preproc.query('sample == 1').drop(['sample', 'Restaurant_id'], axis=1)
+# подготовка тестируемых данных - мы расширили списко по параметры стилей кухонь!!!
 test_data = df_preproc.query('sample == 0').drop(['sample'], axis=1)
+test_data = test_data.drop_duplicates(subset=['Restaurant_id'], keep='first').drop(['Restaurant_id'], axis=1)
+
 
 y = train_data.Rating.values
 X = train_data.drop(['Rating'], axis=1)
@@ -187,10 +212,10 @@ print('MAE:', metrics.mean_absolute_error(y_test, y_pred))
 
 print(pd.Series(model.feature_importances_, index=X.columns).sort_values(ascending=False))
 
-y_pred_1 =  test_data.Rating.values
+
 test_data = test_data.drop(['Rating'], axis=1)
 predict_submission = model.predict(test_data)
 sample_submission['Rating'] = predict_submission
 sample_submission.to_csv('submission.csv', index=False)
 
-print('MAE2:', metrics.mean_absolute_error(y_pred_1, predict_submission))
+# print('MAE2:', metrics.mean_absolute_error(y_pred_1, predict_submission))
