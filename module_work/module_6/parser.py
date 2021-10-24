2efcb4cd0a3017da9ee0732a9d1b0832b2ef885e
daimond025 <daimond025@yandex.ru>
2021-10-24 21:27:31 +0300

    Второй модуль!

--- module_work/module_6/parser.py
+++ module_work/module_6/parser.py
@@ -1,9 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
 
 f = open('./data/Base.csv', 'w')
 # ЗАГОЛОВКИ ДЛЯ ДАННЫх
@@ -24,7 +24,7 @@ def get_page_data(html):
     for car in cars:
         url = car.find('a', class_='ListingItemTitle__link')
 
-        if len(url) >0 and url['href'] != "":
+        if len(url) > 0 and url['href'] != "":
             getCar(url['href'])
 
 
@@ -32,6 +32,46 @@ def getCar(url):
     response = requests.get(url)
     response.encoding = "utf-8"
     car_detail = response.text
+    soup = BeautifulSoup(car_detail, 'lxml')
+
+    bodyType = ''
+
+    print(url)
+    exit()
+    bodyType_li = soup.find('li', class_='CardInfoRow_bodytype')
+    if bodyType_li.find('a'):
+        bodyType = bodyType_li.find('a').text.title()
+
+    brand = ''
+    brand_elem = soup.find('div', class_='BreadcrumbsPopup')
+    if brand_elem.find('a'):
+        brand = brand_elem.find('a').text.title()
+
+    color = ''
+    color_elem = soup.find('li', class_='CardInfoRow_color')
+    if color_elem.find('a'):
+        color = color_elem.find('a').text.title()
+
+    fuelType = ''
+    fuelType_elem = soup.find('li', class_='CardInfoRow_engine')
+    if fuelType_elem.find('a'):
+        fuelType = fuelType_elem.find('a').text.title()
+
+    # modelDate
+    modelDate = ''
+    modelDate_elem = soup.find('li', class_='CardInfoRow_year')
+    if modelDate_elem.find('a'):
+        modelDate = modelDate_elem.find('a').text.title()
+
+    # name
+    name = ''
+
+    #  детальная информация о моделиф
+    url_deteil = soup.find('a', class_='SpoilerLink_type_default')
+    response = requests.get(url_deteil)
+    response.encoding = "utf-8"
+    model_detail = response.text
+
 
 
 
